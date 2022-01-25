26d8db0bf9f185043d002c035b1a4705e65daa4b
Lenovo <Lenovo@sandip>
2022-01-25 13:48:13 +0530

    Dev1 doing changes in logical

--- DemoForGit/src/com/nt/beans/Utility.java
+++ DemoForGit/src/com/nt/beans/Utility.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 public class Utility {
 	public Date getSysDate() {
+		System.out.println("date is shown");
 		return new Date();
+		
 	}
 
 }
--- DemoForGit/src/test/ClassA.java
+++ DemoForGit/src/test/ClassA.java
@@ -4,6 +4,7 @@ public class ClassA {
 private void syso() {
 	// TODO Auto-generated method stub
 	System.out.println("Hello Peter");
+	System.out.println("Hello Sandip");
 
 }
 }
